3266f0992573775b39779ae4d0f53153867d0a1b
James Ravenscroft <ravenscroftj@gmail.com>
2012-09-16 09:22:42 +0100

    Started working on the learning engine using ORM

--- learning.py
+++ learning.py
@@ -5,15 +5,25 @@ by James Ravenscroft
 '''
 import logging
 
+from sqlalchemy.orm import sessionmaker
+from mpdevt import mpd_listener
+
+#database setup stuff
+_Session = sessionmaker()
+
 class LearningEngine(object):
   '''Class that does some clever stuff to help figure out what song to play next
   '''
   
   songs = {}
   
-  def __init__(self):
+  def __init__(self, dbengine):
     self.logger = logging.getLogger("LearningEngine")
+    _Session.configure(bind=dbengine)
+    self.session = _Session()
   
-  
-  def add_track(self, songinfo):
-    pass
+  @mpd_listener('OnSongChange')
+  def OnSongChange(event_type, evt):
+    '''Called when the MPD client finds that a new song is playing
+    '''
+    
--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import mutagen
 import os
-import mpd
 import sys
 import logging
 import pylast
+import threading
+
+from mpdevt import PollingMPDClient, mpd_listener
+
 
 API_KEY    = "21e7e21f732bac4749c6deb03b902cc5"
 API_SECRET = "5d529e71dacfef10ae597a086a435afe"
 
+@mpd_listener('OnSongChange')
+def test_listener(type, evt):
+  logging.info("Now playing: %s - %s", evt['title'], evt['artist'])
+
 if __name__ == "__main__":
-  logging.basicConfig()
+  logging.basicConfig(level=logging.INFO)
+  logging.info("Starting QMPD...")
   
-  
-  if(len(sys.argv) < 2):
+  if(len(sys.argv) > 1):
     
     if(len(sys.argv) > 2):
       port = 6600
     else:
       port = sys.argv[2]
     
-    client = MPDClient()
-    client.connect(host=sys.argv[1], port=port)
+    try:
+      client = PollingMPDClient()
+      client.connect(host=sys.argv[1], port=port)
+      l = raw_input()
+    except KeyboardInterrupt:
+      print "exiting..."
+    
+    client.running = False
   
   else:
     print "Usage: %s <host> [port]" % sys.argv[0]
+    
