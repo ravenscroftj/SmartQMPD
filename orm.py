b3be3472ec0128deaf14538d8aa1e0611f7e60e2
James Ravenscroft <ravenscroftj@gmail.com>
2012-09-16 08:34:46 +0100

    First iteration of database stuff

---
+++ orm.py
@@ -0,0 +1,78 @@
+'''
+
+Database stuff for the clustering application
+
+'''
+import logging
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+#---------------------------------------------------------------
+
+artist_tags = Table('artist_tags', Base.metadata,
+    Column('artist_id', Integer, ForeignKey('artists.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+#---------------------------------------------------------------
+
+album_tags = Table('album_tags', Base.metadata,
+    Column('album_id', Integer, ForeignKey('artists.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+#---------------------------------------------------------------
+
+song_tags = Table('song_tags', Base.metadata,
+    Column('song_id', Integer, ForeignKey('artists.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+  
+#---------------------------------------------------------------
+
+class Tag(Base):
+  
+  __tablename__ = 'tags'
+  
+  id  = Column(Integer, primary_key=True)
+  tag = Column(String)
+
+#---------------------------------------------------------------
+
+class Artist(Base):
+  
+  __tablename__ = 'artists'
+  
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  songs = relationship("Song", backref="artist")
+  albums = relationship("Album", backref="artist")
+  tags = relationship("Tag", secondary=artist_tags, backref="artists")
+  
+#---------------------------------------------------------------
+
+class Album(Base):
+  __tablename__ = 'albums'
+  
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  artist = Column(Integer, ForeignKey('artists.id'))
+  tags = relationship("Tag", secondary=album_tags, backref="albums")
+  
+#---------------------------------------------------------------
+
+class Song(Base):
+  
+  __tablename__ = 'songs'
+  
+  id = Column(Integer, primary_key=True)
+  title  = Column(String)
+  artist = Column(Integer, ForeignKey('artists.id'))
+  album  = Column(Integer, ForeignKey('albums.id'))
+  tags = relationship("Tag", secondary=song_tags, backref="songs")
+  
+#---------------------------------------------------------------
