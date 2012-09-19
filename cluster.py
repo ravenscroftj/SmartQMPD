9a1e878cb16d6fe8370f27e188fe2ca05e9c6446
James Ravenscroft <ravenscroftj@gmail.com>
2012-09-19 15:47:10 +0100

    Written tag distance function

--- cluster.py
+++ cluster.py
@@ -7,4 +7,34 @@ class MusicClusterer(object):
     pass
   
   def cluster(self, objects):
+    
     pass
+  
+  
+class Cluster:
+  
+  def __init__(self, data):
+    self.data = data
+  
+  def distance(self, cluster):
+    tags_1 = self.get_tags()
+    tags_2 = cluster.get_tags()
+    i_1 = float(len(tags_1))
+    i_2 = float(len(tags_2))
+    
+    x = 0.0
+    
+    for t in tags_1:
+      if t in tags_2: x += 1
+    
+    return x / max( i_1, i_2 )
+    
+  def get_tags(self):
+    tags = []
+    
+    for d in self.data:
+      tags.extend(d['tags'])
+  
+    return tags
+  
+  
--- clustertest.py
+++ clustertest.py
@@ -2,12 +2,34 @@ import cluster
 import pylast
 import os
 import json
+import mpd
 from mutagen.easyid3 import EasyID3
+import cluster
 
 API_KEY    = "21e7e21f732bac4749c6deb03b902cc5"
 API_SECRET = "5d529e71dacfef10ae597a086a435afe"
 
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
+
+#----------------------------------------------------------------------------
+
+def gather_mpd_data( ):
+  
+  music_files = []
+  client = mpd.MPDClient()
+  client.connect(host="localhost", port=6600)
+  
+  songs = client.playlistinfo()
+  music_count = len(songs)
+  print "There are %d tracks detected" % music_count
+  
+  i = 1
+  for song in songs:
+    print "[%d%%] %d / %d " % ( i*100/music_count , i, music_count )
+    song['tags'] = get_tags(song['artist'], song['title'])
+    i += 1
+  return songs
+#----------------------------------------------------------------------------
 
 def gather_data( path ):
   
@@ -31,6 +53,8 @@ def gather_data( path ):
     i += 1
   return metadata
 
+#----------------------------------------------------------------------------
+
 def process_file( file_path ):
   '''Process a file and get metadata'''
   metadata = {}
@@ -40,21 +64,33 @@ def process_file( file_path ):
     metadata[key] = e[key][0]
     
   #get the last fm data
-  l_track = network.get_track(e['artist'][0], e['title'][0])
+  metadata['tags'] = get_tags(e['artist'][0], e['title'][0])
+  
+  return metadata
+
+#----------------------------------------------------------------------------
+
+def get_tags(artist , title):
+ 
+  
+  l_track = network.get_track(artist, title)
   
   tags = []
   for tag in l_track.get_top_tags():
     if(tag.weight > 20):
       tags.append(tag.item.name)
-    
-  metadata['tags'] = tags
   
-  return metadata
+  return tags
+
+#----------------------------------------------------------------------------
 
 if __name__ == "__main__":
   
+  
+  
   if not(os.path.exists("/tmp/testdata")):
-    data = gather_data("/home/james/tmp/")
+    data = gather_mpd_data()
+    #data = gather_data("/home/james/tmp/")
     
     with open("/tmp/testdata",'w') as f:
       json.dump(data, f)
@@ -64,5 +100,7 @@ if __name__ == "__main__":
     with open("/tmp/testdata",'r') as f:
       data = json.load(f)
     
-  for d in data:
-    print data
+  c1 = cluster.Cluster([data[0]])
+  c2 = cluster.Cluster([data[1]])
+  
+  print c2.distance(c1)
